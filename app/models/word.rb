529c2ee16b4bd2879ab626f58e7d61d7d66d6a07
fsword <li.jianye@gmail.com>
2012-01-15 22:20:01 +0800

    add validate

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,5 +1,5 @@
 class Word < ActiveRecord::Base
 
   scope :learn_by, lambda{|record_type| where("can_#{record_type}".to_sym => false)}
-  
+  validates_uniqueness_of :foreign_lang, :native_lang 
 end
