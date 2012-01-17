0f17319c428366792329f775c778403b7d3770f7
fsword <li.jianye@gmail.com>
2012-01-17 23:12:08 +0800

    帮助用户隐藏信息

--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -1,2 +1,13 @@
 module WordsHelper
+  def check_foreign
+    session[:filter] == 'foreign'
+  end
+  
+  def check_native
+    session[:filter] == 'native'
+  end
+  
+  def nocheck
+    session[:filter] == nil
+  end
 end
